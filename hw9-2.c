fb3bd46807d40c7f0e5dc61b6ecec95d76f54456
z125529-NOA-ARON <z125529@shibaura-it.ac.jp>
2025-12-05 22:41:44 +0900

    Add files via upload

--- hw9-2.c
+++ hw9-2.c
@@ -18,14 +18,14 @@ int main() {
     printf("You entered\n");
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 3; j++)
-            printf("%4d", a[i][j]);
+            printf("%d ", a[i][j]);
         printf("\n");
     }
 
     printf("Output\n");
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 3; j++)
-            printf("%4d", b[i][j]);
+            printf("%d ", b[i][j]);
         printf("\n");
     }
 
