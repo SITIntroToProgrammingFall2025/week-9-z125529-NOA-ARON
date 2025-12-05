9c0b5b5d315418efec9792b086472e92cc221efd
z125529-NOA-ARON <z125529@shibaura-it.ac.jp>
2025-12-05 23:28:40 +0900

    Add files via upload

--- hw9-3.c
+++ hw9-3.c
@@ -2,39 +2,53 @@
 
 int main() {
     // z125529
+    int rows_A = 2, cols_A = 3;
+    int rows_B = 3, cols_B = 2;
 
-    int A[3][2], B[2][3], C[3][3];
+    double A[2][3], B[3][2], C[2][2];
 
-    for(int i = 0; i < 3; i++)
-        for(int j = 0; j < 2; j++)
-            scanf("%d", &A[i][j]);
+    for(int i = 0; i < rows_A; i++) {
+        for(int j = 0; j < cols_A; j++) {
+            scanf("%lf", &A[i][j]);
+        }
+    }
 
-    for(int i = 0; i < 2; i++)
-        for(int j = 0; j < 3; j++)
-            scanf("%d", &B[i][j]);
+    for(int i = 0; i < rows_B; i++) {
+        for(int j = 0; j < cols_B; j++) {
+            scanf("%lf", &B[i][j]);
+        }
+    }
 
     printf("The first matrix you entered is\n");
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 2; j++)
-            printf("%4d", A[i][j]);
+    for(int i = 0; i < rows_A; i++) {
+        for(int j = 0; j < cols_A; j++) {
+            printf("%g ", A[i][j]);
+        }
         printf("\n");
     }
 
     printf("The second matrix you entered is\n");
-    for(int i = 0; i < 2; i++) {
-        for(int j = 0; j < 3; j++)
-            printf("%4d", B[i][j]);
+    for(int i = 0; i < rows_B; i++) {
+        for(int j = 0; j < cols_B; j++) {
+            printf("%g ", B[i][j]);
+        }
         printf("\n");
     }
 
-    for(int i = 0; i < 3; i++)
-        for(int j = 0; j < 3; j++)
-            C[i][j] = A[i][0]*B[0][j] + A[i][1]*B[1][j];
+    for(int i = 0; i < rows_A; i++) {
+        for(int j = 0; j < cols_B; j++) {
+            C[i][j] = 0;
+            for(int k = 0; k < cols_A; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
 
     printf("The multiplication product of matrix A and matrix B:\n");
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++)
-            printf("%4d", C[i][j]);
+    for(int i = 0; i < rows_A; i++) {
+        for(int j = 0; j < cols_B; j++) {
+            printf("%g ", C[i][j]);
+        }
         printf("\n");
     }
 
